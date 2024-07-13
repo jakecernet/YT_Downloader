67c05f2bcd2e57d2aa4b29c4302385e696d485c6
Jaka Černetič <jaka.cernetic@students.vegova.si>
2024-07-13 12:25:33 +0200

    added a simplified cli version which downloads at 192kbits

---
+++ main-simple.py
@@ -0,0 +1,43 @@
+import os
+from turtle import title
+from yt_dlp import YoutubeDL
+title = "YouTube Downloader"
+
+def download_video(url):
+    try:
+        # Set options for yt_dlp
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'ignoreerrors': True,
+        }
+
+        # Create a YoutubeDL object
+        ydl = YoutubeDL(ydl_opts)
+
+        # Download the video and extract audio as MP3
+        info_dict = ydl.extract_info(url, download=True)
+        video_title = info_dict.get('title', None)
+
+        print("Video downloaded as MP3 successfully!")
+
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+
+if __name__ == "__main__":
+    print("--------------------------------------")
+    print("  Welcome to the YouTube Downloader!")
+    print("--------------------------------------")
+    print("")
+    print("---------------------------------")
+    video_url = input("  Enter the YouTube video URL: ")
+    print("---------------------------------")
+    print("Downloading...")
+    download_video(video_url)
+    print("---------------------------------")
+    print("Thank you for using the YouTube Downloader!")
