cd1ec6f666242497baa509a7a902f1f86c1c4f8e
Jaka Černetič <jaka.cernetic@gmail.com>
2024-12-14 13:34:10 +0100

    izbolšan ui

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import customtkinter
 from yt_dlp import YoutubeDL
 
@@ -47,6 +46,7 @@ def downloadAudio(choice):
 
 def downloadVideo(choice):
     ydl_opts = {
+        'outtmpl': '/%(title)s.%(ext)s',
         'format': 'bestvideo[height<={}]'.format(choice) + '+bestaudio/best',
         'postprocessors': [{ 
             'key': 'FFmpegVideoConvertor', 
@@ -56,6 +56,9 @@ def downloadVideo(choice):
         'restrictfilenames': True, 
         'ignoreerrors': True,
         'quiet': True,
+        'writesubtitles': True,
+        'allsubtitles': True,
+        'subtitlesformat': 'srt',
     }
 
     with YoutubeDL(ydl_opts) as ydl:
@@ -77,64 +80,3 @@ video = customtkinter.CTkButton(master=app, text="Video", command=video_download
 video.place(relx=0.8, rely=0.3, anchor=customtkinter.CENTER)
 
 app.mainloop()
-
-""" def download(url):
-    format = input("Do you want to download audio or video? (Audio - 1, Video - 2) ")
-    
-    if format == "1":
-        convert = input("Do you want to convert the audio MP3? (Y, N) ")
-        quality = input("What quality do you want to convert the audio to? (128, 192, 256, 320) ")
-
-        if convert == "Y":
-            keep = input("Do you want to keep the original audio file? (Y, N) ")
-            if keep == "Y":
-                ydl_opts = {
-                    'format': 'bestaudio/best',
-                    'postprocessors': [{
-                        'key': 'FFmpegExtractAudio',
-                        'preferredcodec': 'mp3',
-                        'preferredquality': quality,
-                    }],
-                    'keepvideo': True,
-                }
-            else:
-                ydl_opts = {
-                    'format': 'bestaudio/best',
-                    'postprocessors': [{
-                        'key': 'FFmpegExtractAudio',
-                        'preferredcodec': 'mp3',
-                        'preferredquality': quality,
-                    }],
-                    'keepvideo': False,
-                }
-        else:
-            ydl_opts = {
-                'format': 'bestaudio/best',
-                'preferredquality': quality,
-            }
-
-    elif format == "2":
-        convert = input("Do you want to convert the video to MP4? (Y, N) ")
-        quality = input("What quality do you want to convert the video to? (144, 240, 360, 480, 720, 1080, 1440, 2160) ")
-
-        if convert == "Y":
-            ydl_opts = {
-                'format': 'bestvideo[height<={}]'.format(quality) + '+bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegVideoConvertor',
-                    'preferedformat': 'mp4',
-                }],
-            }
-        else:
-            ydl_opts = {
-                'format': 'bestvideo[height<={}]'.format(quality) + '+bestaudio/best',
-            }
-        
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    
-def main():
-    url = input("Enter the URL of the video you want to download: ")
-    download(url)
-
-main() """
